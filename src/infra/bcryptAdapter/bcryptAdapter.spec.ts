6030fbfa6a2b66185de1f83084be7035595d7739
sahydi <sahydi@protonmail.com>
2025-03-08 14:03:56 -0300

    feat: Ensure BcryptAdapter return a hash if valid data is provided

--- src/infra/bcryptAdapter/bcryptAdapter.spec.ts
+++ src/infra/bcryptAdapter/bcryptAdapter.spec.ts
@@ -1,25 +1,29 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from '@/infra';
 
-// bcrypt.hash = jest.fn().mockResolvedValue('hashedValue');
-
 type SutTypes = {
   sut: BcryptAdapter;
 };
 
+const SALT = 12;
 const makeSut = (): SutTypes => {
-  const SALT = 12;
   const sut = new BcryptAdapter(SALT);
   return {
     sut,
   };
 };
 
+bcrypt.hash = jest.fn().mockResolvedValue('hashedPassword');
 describe('BcryptAdapter', () => {
   test('Should call Bcrypt with correct password', async () => {
     const { sut } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.hash('anyPassword');
-    expect(hashSpy).toHaveBeenCalledWith('anyPassword', 12);
+    expect(hashSpy).toHaveBeenCalledWith('anyPassword', SALT);
+  });
+  test('Should return hashedPassword if valid data is provided', async () => {
+    const { sut } = makeSut();
+    const hashedPassword = await sut.hash('anyPassword');
+    expect(hashedPassword).toEqual('hashedPassword');
   });
 });
