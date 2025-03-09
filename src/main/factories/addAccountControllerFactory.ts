5b5b086b00d8f5d44f49c745f41bca92438b9a9b
sahydi <sahydi@protonmail.com>
2025-03-09 15:05:53 -0300

    feat: add AddAccountControllerFactory

---
+++ src/main/factories/addAccountControllerFactory.ts
@@ -0,0 +1,14 @@
+import { AddAccountToDatabase } from '@/application';
+import { BcryptAdapter, MongoDbAdapter } from '@/infra';
+import { AddAccountController } from '@/presentation';
+import { EmailValidatorAdapter } from '@/utils';
+
+export const addAccountControllerFactory = (): AddAccountController => {
+  const SALT = 12;
+  const mongoDbAdapter = new MongoDbAdapter();
+  const bcryptAdapter = new BcryptAdapter(SALT);
+  const addAccountToDatabase = new AddAccountToDatabase(bcryptAdapter, mongoDbAdapter);
+  const emailValidator = new EmailValidatorAdapter();
+  const addAccountController = new AddAccountController(emailValidator, addAccountToDatabase);
+  return addAccountController;
+};
