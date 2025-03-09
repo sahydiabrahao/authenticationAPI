985b57c154e05c45bfff1b795ccb29490828bdc7
sahydi <sahydi@protonmail.com>
2025-03-09 14:14:28 -0300

    refactor: Ensure AddAccount connect with MongoDB before each test

--- src/main/routes/addAccountRoutes.test.ts
+++ src/main/routes/addAccountRoutes.test.ts
@@ -1,7 +1,19 @@
 import request from 'supertest';
-import app from '../../main';
+import app from '@/main';
+import { MongoHelper } from '@/infra';
 
 describe('AddAccout Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/jest');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await MongoHelper.clearAllCollection();
+  });
   test('Should return account on success', async () => {
     await request(app)
       .post('/api/addAccount')
