0f34497f08248ac5c8db2f043df85017abcd7830
sahydi <sahydi@protonmail.com>
2025-03-15 19:51:43 -0300

    feat: Ensure ValidatorComposite return the first error if more than one validation fails

--- src/presentation/validator/validatorComposite/validatorComposite.spec.ts
+++ src/presentation/validator/validatorComposite/validatorComposite.spec.ts
@@ -1,4 +1,5 @@
 import {
+  InvalidParamError,
   MissingParamError,
   ValidatorComposite,
   ValidatorInput,
@@ -6,20 +7,23 @@ import {
   ValidatorOutput,
 } from '@presentation';
 
-export class ValidatorStub implements ValidatorModel {
-  validate(input: ValidatorInput): ValidatorOutput {
-    return null;
+const makeValidatorStub = (): ValidatorModel => {
+  class ValidatorStub implements ValidatorModel {
+    validate(input: ValidatorInput): ValidatorOutput {
+      return null;
+    }
   }
-}
+  return new ValidatorStub();
+};
 
 type SutTypes = {
   sut: ValidatorComposite;
-  validatorStub: ValidatorModel;
+  validatorStub: ValidatorModel[];
 };
 
 const makeSut = (): SutTypes => {
-  const validatorStub = new ValidatorStub();
-  const sut = new ValidatorComposite([validatorStub]);
+  const validatorStub = [makeValidatorStub(), makeValidatorStub()];
+  const sut = new ValidatorComposite(validatorStub);
   return {
     sut,
     validatorStub,
@@ -29,9 +33,16 @@ const makeSut = (): SutTypes => {
 describe('ValidatorComposite', () => {
   test('Should return an error if any validation fails', () => {
     const { sut, validatorStub } = makeSut();
-    jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new MissingParamError('anyField'));
+    jest.spyOn(validatorStub[0], 'validate').mockReturnValueOnce(new MissingParamError('anyField'));
     const output = sut.validate({ anyField: 'anyValue' });
     expect(output).toEqual(new MissingParamError('anyField'));
+  });
+  test('Should return the first error if more than one validation fails', () => {
+    const { sut, validatorStub } = makeSut();
+    jest.spyOn(validatorStub[0], 'validate').mockReturnValueOnce(new InvalidParamError('anyField'));
+    jest.spyOn(validatorStub[1], 'validate').mockReturnValueOnce(new MissingParamError('anyField'));
+    const output = sut.validate({ anyField: 'anyValue' });
+    expect(output).toEqual(new InvalidParamError('anyField'));
   });
   test('Should return null if validation success', () => {
     const { sut } = makeSut();
