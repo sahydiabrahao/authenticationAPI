abe9bf3bd112018aa5e13d7d018a51160078e5bb
sahydi <sahydi@protonmail.com>
2025-03-15 19:43:05 -0300

    feat: Ensure ValidatorComposite return an error if any validation fails

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -9,10 +9,10 @@ export * from './authenticateAccountController/authenticateAccountController';
 export * from './validator/protocols/validatorInput';
 export * from './validator/protocols/validatorOutput';
 export * from './validator/protocols/validatorModel';
-export * from './validator/validatorComposite';
-export * from './validator/requiredFieldValidator';
-export * from './validator/compareFieldValidator';
-export * from './validator/emailValidator';
+export * from './validator/validatorComposite/validatorComposite';
+export * from './validator/requiredFieldValidator/requiredFieldValidator';
+export * from './validator/compareFieldValidator/compareFieldValidator';
+export * from './validator/emailValidator/emailValidator';
 
 export * from './errors/missingParamError';
 export * from './errors/invalidParamError';
---
+++ src/presentation/validator/validatorComposite/validatorComposite.spec.ts
@@ -0,0 +1,36 @@
+import {
+  MissingParamError,
+  ValidatorComposite,
+  ValidatorInput,
+  ValidatorModel,
+  ValidatorOutput,
+} from '@presentation';
+
+export class ValidatorStub implements ValidatorModel {
+  validate(input: ValidatorInput): ValidatorOutput {
+    return null;
+  }
+}
+
+type SutTypes = {
+  sut: ValidatorComposite;
+  validatorStub: ValidatorModel;
+};
+
+const makeSut = (): SutTypes => {
+  const validatorStub = new ValidatorStub();
+  const sut = new ValidatorComposite([validatorStub]);
+  return {
+    sut,
+    validatorStub,
+  };
+};
+
+describe('ValidatorComposite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validatorStub } = makeSut();
+    jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new MissingParamError('anyField'));
+    const output = sut.validate({ anyField: 'anyValue' });
+    expect(output).toEqual(new MissingParamError('anyField'));
+  });
+});
