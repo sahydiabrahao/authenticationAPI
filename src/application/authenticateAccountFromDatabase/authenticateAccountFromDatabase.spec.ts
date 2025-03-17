aa58a00aa9cd274ecace94f807a9f9456baec674
sahydi <sahydi@protonmail.com>
2025-03-17 08:55:56 -0300

    Ensure AuthenticateAccountFromDatabase throws if LoadAccountByEmail throw an error

--- src/application/authenticateAccountFromDatabase/authenticateAccountFromDatabase.spec.ts
+++ src/application/authenticateAccountFromDatabase/authenticateAccountFromDatabase.spec.ts
@@ -29,14 +29,22 @@ const makeSut = (): SutTypes => {
 };
 
 describe('AuthenticateAccountFromDatabase', () => {
-  test('Should call LoadAccountByEmail with correct email', () => {
+  test('Should call LoadAccountByEmail with correct email', async () => {
     const { sut, loadAccountByEmailStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByEmailStub, 'load');
-    sut.auth({
+    await sut.auth({
       email: 'anyEmail@mail.com',
       password: 'anyPassword',
     });
-
     expect(loadSpy).toHaveBeenCalledWith({ email: 'anyEmail@mail.com' });
+  });
+  test('Should throws if LoadAccountByEmail throw an error', async () => {
+    const { sut, loadAccountByEmailStub } = makeSut();
+    jest.spyOn(loadAccountByEmailStub, 'load').mockReturnValueOnce(Promise.reject(new Error()));
+    const promise = sut.auth({
+      email: 'anyEmail@mail.com',
+      password: 'anyPassword',
+    });
+    expect(promise).rejects.toThrow();
   });
 });
